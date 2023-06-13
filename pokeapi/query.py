ed54ec6fef0aa36c59ff1f0a43b4467180d905fc
kohdice <kohdice@gmail.com>
2023-06-13 21:38:52 +0900

    Fix create_param()

--- pokeapi/query.py
+++ pokeapi/query.py
@@ -4,5 +4,5 @@ from typing import Any
 
 class Param(ABC):
     @abstractmethod
-    def create_param(self, target: str) -> dict[str, Any] | None:
+    def create_param(self) -> dict[str, Any] | None:
         pass
