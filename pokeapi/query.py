14c98dd7a5caeda582059ddee63a85c3d0f40d56
kohdice <kohdice@gmail.com>
2023-06-13 22:59:15 +0900

    Add `CreatePokedexNumberParam` class

--- pokeapi/query.py
+++ pokeapi/query.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from typing import Any
 
 
@@ -6,3 +7,18 @@ class Param(ABC):
     @abstractmethod
     def create_param(self) -> dict[str, Any] | None:
         pass
+
+
+@dataclass
+class CreatePokedexNumberParam(Param):
+    pokedex_number: str | None
+
+    def create_param(self) -> dict[str, Any] | None:
+        if self.pokedex_number is None:
+            return None
+        try:
+            pokedex_number = int(self.pokedex_number)
+        except ValueError:
+            return None
+
+        return {"term": {"national_pokedex_number": {"value": pokedex_number}}}
--- tests/test_query.py
+++ tests/test_query.py
@@ -22,3 +22,23 @@ class TestAbstractParam:
     def test_not_implemented(self) -> None:
         with pytest.raises(TypeError):
             self.ErrorSubParam()
+
+
+class TestCreatePokedexNumberParam:
+    def test_create_pokedex_number_param(self) -> None:
+        q = query.CreatePokedexNumberParam("1")
+        actual = q.create_param()
+
+        assert actual == {"term": {"national_pokedex_number": {"value": 1}}}
+
+    def test_create_pokedex_number_param_not_number(self) -> None:
+        q = query.CreatePokedexNumberParam("test")
+        actual = q.create_param()
+
+        assert actual is None
+
+    def test_create_pokedex_number_param_none(self) -> None:
+        q = query.CreatePokedexNumberParam(None)
+        actual = q.create_param()
+
+        assert actual is None
