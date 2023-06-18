b9272375001eb2273f3b6228a367c1957fe21562
kohdice <kohdice@gmail.com>
2023-06-18 18:23:47 +0900

    Add a route to search for Pokémon by Pokédex number

--- pokeapi/routers/pokemon.py
+++ pokeapi/routers/pokemon.py
@@ -11,3 +11,10 @@ async def read_pokemon() -> dict[str, str]:
 @router.get("/pokemon/name/{name}")
 async def read_pokemon_by_name(name: str) -> dict[str, str]:
     return {"Name": name}
+
+
+@router.get("/pokemon/pokedex_number/{pokedex_number}")
+async def read_pokemon_by_pokedex_number(
+    pokedex_number: str,
+) -> dict[str, int | str]:
+    return {"national_pokedex_number": pokedex_number, "name": "けつばん"}
--- tests/test_pokemon.py
+++ tests/test_pokemon.py
@@ -24,3 +24,20 @@ class TestPokemon:
 
         assert responce.status_code == 404
         assert responce.json() == {"detail": "Not Found"}
+
+    def test_read_pokemon_by_pokedex_number(self) -> None:
+        client = TestClient(main.app)
+        responce = client.get("/pokemon/pokedex_number/152")
+
+        assert responce.status_code == 200
+        assert responce.json() == {
+            "national_pokedex_number": 152,
+            "name": "けつばん",
+        }
+
+    def test_read_pokemon_by_pokedex_number_none(self) -> None:
+        client = TestClient(main.app)
+        responce = client.get("/pokemon/pokedex_number/")
+
+        assert responce.status_code == 404
+        assert responce.json() == {"detail": "Not Found"}
