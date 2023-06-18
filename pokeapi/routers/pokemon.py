4d8973e792567b493424b0d775244fbfaa85d53e
kohdice <kohdice@gmail.com>
2023-06-18 18:13:37 +0900

    Add a route to search for Pokémon by name

--- pokeapi/routers/pokemon.py
+++ pokeapi/routers/pokemon.py
@@ -6,3 +6,8 @@ router = APIRouter()
 @router.get("/pokemon")
 async def read_pokemon() -> dict[str, str]:
     return {"Name": "けつばん"}
+
+
+@router.get("/pokemon/name/{name}")
+async def read_pokemon_by_name(name: str) -> dict[str, str]:
+    return {"Name": name}
--- tests/test_pokemon.py
+++ tests/test_pokemon.py
@@ -10,3 +10,17 @@ class TestPokemon:
 
         assert responce.status_code == 200
         assert responce.json() == {"Name": "けつばん"}
+
+    def test_read_pokemon_by_name(self) -> None:
+        client = TestClient(main.app)
+        responce = client.get("/pokemon/name/けつばん")
+
+        assert responce.status_code == 200
+        assert responce.json() == {"Name": "けつばん"}
+
+    def test_read_pokemon_by_name_none(self) -> None:
+        client = TestClient(main.app)
+        responce = client.get("/pokemon/name/")
+
+        assert responce.status_code == 404
+        assert responce.json() == {"detail": "Not Found"}
