44d7842a5ab4ad7503293f4aec49263331f7c216
kohdice <kohdice@gmail.com>
2023-06-22 22:41:14 +0900

    Fix test codes

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,13 @@ import pytest
 
 @pytest.fixture()
 def _setup_get_config() -> Generator[None, None, None]:
+    url = "http://localhost:9200"
+
+    if os.getenv("STAGE") == "development":
+        url = "http://elasticsearch:9200"
+
     os.environ["ES_INDEX"] = "pokemon"
-    os.environ["ES_CONNECTION_URL"] = "http://localhost:9200"
+    os.environ["ES_CONNECTION_URL"] = url
     yield
     del os.environ["ES_INDEX"]
     del os.environ["ES_CONNECTION_URL"]
