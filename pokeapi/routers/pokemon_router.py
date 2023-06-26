7bd2bddafb7e8c8cdf1568384d14b11b509cd606
kohdice <kohdice@gmail.com>
2023-06-26 15:59:36 +0900

    Implementation of `read_pokemon_by_conditions()`

--- pokeapi/routers/pokemon_router.py
+++ pokeapi/routers/pokemon_router.py
@@ -3,9 +3,25 @@ from typing import Annotated
 
 from fastapi import APIRouter, Path, Query
 
+from pokeapi.search.param import (
+    CreateAbilityParam,
+    CreateFormParam,
+    CreateGenderTypeParam,
+    CreateLegendaryParam,
+    CreateMegaEvolutionParam,
+    CreateMythicalParam,
+    CreatePokemonTypeParam,
+    CreatePrimalReversionParam,
+    CreateRegionalVariantParam,
+)
+
 from ..schemas.pokemon_schema import PokemonSchema
 from ..search import accessor
-from ..search.query import CreatePokedexNumberQuery, CreatePokemonNameQuery
+from ..search.query import (
+    CreateConditionalSearchQuery,
+    CreatePokedexNumberQuery,
+    CreatePokemonNameQuery,
+)
 
 router = APIRouter()
 
@@ -73,12 +89,37 @@ async def read_pokemon_by_pokedex_number(
     return accessor.create_pokemon_response(es_response)
 
 
-@router.get("/pokemon/conditions")
+@router.get(
+    "/pokemon/conditions", response_model=list[PokemonSchema] | dict[str, str]
+)
 async def read_pokemon_by_conditions(
-    condition: str | int,
-) -> dict[str, int | str]:
-    return {
-        "national_pokedex_number": 152,
-        "name": "けつばん",
-        "condition": condition,
-    }
+    ability_1: str | None = None,
+    ability_2: str | None = None,
+    hidden_ability: str | None = None,
+    form: str | None = None,
+    has_male: bool | None = None,
+    has_female: bool | None = None,
+    is_legendary: bool | None = None,
+    is_mega_evolution: bool | None = None,
+    is_mythical: bool | None = None,
+    type_1: str | None = None,
+    type_2: str | None = None,
+    is_primal_reversion: bool | None = None,
+    regional_variant: str | None = None,
+) -> list[PokemonSchema] | dict[str, str]:
+    request_params = (
+        CreateAbilityParam((ability_1, ability_2, hidden_ability)),
+        CreateFormParam(form),
+        CreateGenderTypeParam((has_male, has_female)),
+        CreateLegendaryParam(is_legendary),
+        CreateMegaEvolutionParam(is_mega_evolution),
+        CreateMythicalParam(is_mythical),
+        CreatePokemonTypeParam((type_1, type_2)),
+        CreatePrimalReversionParam(is_primal_reversion),
+        CreateRegionalVariantParam(regional_variant),
+    )
+
+    query = CreateConditionalSearchQuery().create_query(request_params)
+    es_response = accessor.search_pokemon(query)
+
+    return accessor.create_pokemon_response(es_response)
