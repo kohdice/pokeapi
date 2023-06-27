ea70df59884a03ab89d2036ca381e528375709ad
kohdice <kohdice@gmail.com>
2023-06-27 23:07:33 +0900

    Fix output destination of html

--- docs/doc.py
+++ docs/doc.py
@@ -35,5 +35,5 @@ HTML_TEMPLATE = """<!DOCTYPE html>
 """
 
 if __name__ == "__main__":
-    with open("docs/index.html", "w") as fd:
+    with open("docs/static/index.html", "w") as fd:
         print(HTML_TEMPLATE % json.dumps(app.openapi()), file=fd)
